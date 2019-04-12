2906da04e34c67fdf565937f7f90d6f83cf508ac
negitoyo <negishitoyokazu@toyo-pc-2.local>
2019-04-12 17:11:06 +0900

    first

---
+++ htdocs/staff/staff_add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+    <h2>スタッフ追加</h2>
+    <form action="staff_add_check.php" method="post">
+      <h4>スタッフ名を入力してください</h4>
+      <input type="text" name="name" style="width:200px">
+      <h4>パスワードを入力してください</h4>
+      <input type="password" name="pass" style="width:200px">
+      <h4>パスワードをもう一度入力してください</h4>
+      <input type="password" name="pass2" style="width:200px">
+      <br />
+      <input type="button" onclick="history.back()" value="戻る">
+      <input type="submit" value="OK">
+    </form>
+  </body>
+</html>
---
+++ htdocs/staff/staff_add_check.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+    <?php
+      $staff_name = $_POST['name'];
+      $staff_pass = $_POST['pass'];
+      $staff_pass2 = $_POST['pass2'];
+
+      $staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8')
+
+
+
+
+
+
+
+
+
+
+     ?>
+  </body>
+</html>
